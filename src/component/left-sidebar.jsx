3d9c4c4e859ee96a955e8b250839f2dc78668fef
Qofy <148817944+Qofy@users.noreply.github.com>
2024-09-30 12:36:45 +0200

    making correction1

--- src/component/left-sidebar.jsx
+++ src/component/left-sidebar.jsx
@@ -1,43 +1,42 @@
+import '../stylesheet/left-sidebar.css';
 import img1 from '../images/img1.webp';
 import img2 from '../images/img2.webp';
-import '../stylesheet/left-sidebar.css';
+
+ let newsItem = [
+  {
+    id: 1,
+    img: img1,
+    title: `'We just had to flee:' Fear and tension in Lebanon under deadly Israeli bombardment`,
+    desc: 'Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group’s weapons.',
+    date: '29 day ago | Middle East',
+},
+{
+  id: 2,
+  img: img2,
+  title: 'Israel air strikes kill 492 people in Lebanon, including 100 children',
+  desc: 'Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group’s weapons.',
+  date: '29 day ago | Middle East',
+},
+
+]
+
 export default function LeftSidebar() {
   return (
     <div className="left-sidebar">
-      <div className="news-1">
-        <div className="img1">
-         <img src={img1} alt="" />
+     {newsItem.map((item) => (
+        <div className="news-item" key={item.id}>
+          <div className="news-item-img">
+            <img src={item.img} alt={item.title} />
+          </div>
+          <div className="news-item-content">
+            <a href="#">
+              <h3>{item.title}</h3>
+            </a>
+            <p className="headline">{item.desc}</p>
+            <p className="day">{item.date}</p>
+          </div>
         </div>
-        <a href="#">
-          <h3>
-          'We just had to flee': Fear and tension in Lebanon under deadly Israeli bombardment
-          </h3>
-        </a>
-        <p className='headline'>
-        Thousands have fled southern Lebanon while others say they are leaving the capital Beirut.
-        </p>
-        <p className="day">
-        1 day ago | <p className="location">Middle East</p>
-        </p>
-
-      </div>
-
-      <div className="news-2">
-      <div className="img2">
-         <img src={img2} alt="" />
-        </div>
-        <a href="#">
-          <h3>
-          India anger over alleged sexual assault on woman inside police station
-          </h3>
-        </a>
-        <p className='headline'>
-        A video of the woman, detailing her alleged abuse by police, has gone viral and led to an outcry in India.
-        </p>
-        <p className="day">
-        3 day ago | <p className="location">Asia</p>
-        </p>
-      </div>
+      ))}
     </div>
   );
 }
--- src/component/right-sidebar.jsx
+++ src/component/right-sidebar.jsx
@@ -1,66 +1,51 @@
 import '../stylesheet/right-sidebar.css';
-import {Play} from "lucide-react";
+import {Play} from 'lucide-react';
+
+let newsItem = [
+  {
+    id: 1,
+    title: `'We just had to flee:' Fear and tension in Lebanon under deadly Israeli bombardment`,
+    desc: 'Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group’s weapons.',
+    date: '29 day ago | Middle East',
+},
+{
+  id: 2,
+  title: 'Israel air strikes kill 492 people in Lebanon, including 100 children',
+  desc: 'Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group’s weapons.',
+  date: '29 day ago | Middle East',
+},
+{
+  id: 3,
+  title: 'Israel air strikes kill 492 people in Lebanon, including 100 children',
+  desc: 'Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group’s weapons.',
+  date: '29 day ago | Middle East',
+},
+{
+  id: 4,
+  title: 'Israel air strikes kill 492 people in Lebanon, including 100 children',
+  desc: `Israel says it hit 1,600 Hezbollah targets and tells civilians to flee areas near the group's weapons.`,
+  date: '29 day ago | Middle East',
+  icon: Play
+}
+]
 
 export default function RightSidebar() {
   return (
     <div className="right-sidebar">
-      <div className="nws-1">
-      <a href="#">
-        <h3>
-        CrowdStrike: Company to face questions over global IT outage
-        </h3>
-      </a>
-      <p className="headline">
-      Cancer delays, holidays ruined, businesses out of pocket - the CrowdStrike outage examined.
-      </p>
-      <p className="day">
-        9 hrs ago | <p className="location">Technology</p>
-        </p>
-      </div>
-
-      <div className="nws-1">
-      <a href="#">
-        <h3>
-          <Play style={{color: 'black'}} />
-      Astronauts return to Earth after longest stay on ISS
-        </h3>
-      </a>
-      <p className="headline">
-      One Nasa astronaut and two Russian cosmonauts, who spent 374 days in space, were aboard the shuttle.
-      </p>
-      <p className="day">
-        8 hrs ago | <p className="location">World</p>
-        </p>
-      </div>
-
-      <div className="nws-1">
-      <a href="#">
-        <h3>
-        Ex-minister found guilty in case that gripped Singapore
-        </h3>
-      </a>
-      <p className="headline">
-      S Iswaran, best known for bringing the F1 to Singapore, pleaded guilty to receiving gifts while in office.
-      </p>
-      <p className="day">
-        2 hrs ago | <p className="location">Asia</p>
-        </p>
-      </div>
-
-      <div className="nws-1">
-      <a href="#">
-        <h3>
-        Listen: The Global Story - America and the world
-        </h3>
-      </a>
-      <p className="headline">
-      What is the US's relationship with the world today, and how would Donald Trump and Kamala Harris shape it?
-      </p>
-      <p className="day">
-        9 hrs ago | <p className="location">Technology</p>
-        </p>
-      </div>
-
+      {newsItem.map((item) => (
+        <div className="news-item" key={item.id}>
+          <div className="news-item-content">
+            <a href="#">
+              <h3>
+                {item.icon && <item.icon />}
+                {item.title}
+              </h3>
+            </a>
+            <p className="headline">{item.desc}</p>
+            <p className="day">{item.date}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
